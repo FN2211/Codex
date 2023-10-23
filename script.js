6470301e78db8a3a18c8934ed9ec398c18290b8d
FN2211 <91390444+FN2211@users.noreply.github.com>
2023-10-23 11:30:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -21,3 +21,20 @@ pageTurnBtn.forEach((el, index) => {
         }
     }
 })
+
+
+//general control btn clicked
+const pages = document.querySelectorAll('.book-page.page-right');
+const GeneralControlBtn = document.getElementById('gneralControl');
+
+GeneralControlBtn.onclick = () => {
+    pages.forEach((page, index) => {
+        setTimeout(() => {
+            page.classList.add('turn');
+
+            setTimeout(() => {
+                page.style.zIndex = 20 + index;
+            }, 500)
+        }, (index + 1) * 200+ 100)
+    })
+}
