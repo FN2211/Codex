dcf2c0a4ec02a86810bf640d9cf5dc4e2b674aef
FN2211 <91390444+FN2211@users.noreply.github.com>
2023-10-23 12:20:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ pageTurnBtn.forEach((el, index) => {
         else {
             pageTurn.classList.add('turn');
             setTimeout(() => {
-                pageTurn.style.zIndex = 20 - index;
+                pageTurn.style.zIndex = 20 + index;
             }, 500);
         }
     }
@@ -29,12 +29,49 @@ const GeneralControlBtn = document.getElementById('gneralControl');
 
 GeneralControlBtn.onclick = () => {
     pages.forEach((page, index) => {
-        setTimeout(() => {
-            page.classList.add('turn');
-
+        if (index < 1) {
             setTimeout(() => {
-                page.style.zIndex = 20 + index;
-            }, 500)
-        }, (index + 1) * 200+ 100)
+                page.classList.add('turn');
+    
+                setTimeout(() => {
+                    page.style.zIndex = 20 + index;
+                }, 500)
+            }, (index + 1) * 200 + 100)
+        }
     })
 }
+
+
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex() {
+    pageNumber--;
+    if (pageNumber < 0) {
+        pageNumber = totalPages - 1;
+    }
+}
+
+
+//opening Anim
+const coverRight = document.querySelector('.cover.cover-right');
+
+setTimeout(() => {
+    coverRight.classList.add('turn');
+}, 2100)
+
+setTimeout(() => {
+    coverRight.style.zIndex = -1;
+}, 2800)
+
+pages.forEach((_, index) => {
+    setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].classList.remove('turn');
+
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].style.zIndex = 10 + index;
+        }, 500)
+    }, (index + 1) * 200 + 2100)
+})
